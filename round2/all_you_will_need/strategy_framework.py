7632f73f25176cbcfa7097ec2c4d8f8a25f3f176
pvpswaghd <44018990+pvpswaghd@users.noreply.github.com>
2023-04-13 16:03:21 +0800

    changed model to lightgbm
    still bugs for df_res, may need further discussion

--- round2/all_you_will_need/strategy_framework.py
+++ round2/all_you_will_need/strategy_framework.py
@@ -2,21 +2,31 @@
 import pandas as pd
     
 # The addresses for the train data inputs (NO NEED TO ADJUST IT)
-from config import Path
-historical_data_path = Path.historical_data_path
-MARKET_DATA_PATH = f'{historical_data_path}/second_round_train_market_data.csv'
-FUNDAMENTAL_DATA_PATH = f'{historical_data_path}/second_round_train_fundamental_data.csv'
-RETURN_DATA_PATH = f'{historical_data_path}/second_round_train_return_data.csv'
+# from config import Path
+# historical_data_path = Path.historical_data_path
+MARKET_DATA_PATH = f'second_round_datasets/second_round_train_market_data.csv'
+FUNDAMENTAL_DATA_PATH = f'second_round_datasets/second_round_train_fundamental_data.csv'
+RETURN_DATA_PATH = f'second_round_datasets/second_round_train_return_data.csv'
+# just for testing: don't know whether it works or not at this moment
 
 ###################################################################################################################
 # TODO: The following addresses are for your own test only; pls remove it before you submit your code
-MARKET_DATA_PATH = ""
-FUNDAMENTAL_DATA_PATH = ""
-RETURN_DATA_PATH = ""
+# MARKET_DATA_PATH = ""
+# FUNDAMENTAL_DATA_PATH = ""
+# RETURN_DATA_PATH = ""
 ###################################################################################################################
 
 ###################################################################################################################
 # TODO: Import packages you will need, and realize any initialization you will need
+import copy
+import time
+import numpy as np
+import random
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+import talib as ta
+import lightgbm as lgb
+from hyperopt import fmin, hp, partial, tpe, Trials
 
 ###################################################################################################################
 
@@ -43,3 +53,18 @@ def get_decisions(market_df: pd.DataFrame, fundamental_df: pd.DataFrame):
 
 ###################################################################################################################
 # TODO: Write all functions that you will need below
+def data_preparation():
+    df_fund = pd.read_csv(FUNDAMENTAL_DATA_PATH_TEST)
+    df_market = pd.read_csv(MARKET_DATA_PATH_TEST)
+    df_train_fund = pd.read_csv(FUNDAMENTAL_DATA_PATH)
+    df_train_market = pd.read_csv(MARKET_DATA_PATH)
+    df_train_returns = pd.read_csv(RETURN_DATA_PATH)
+
+    df_list = [df_fund, df_market, df_train_fund, df_train_market, df_train_returns]
+    df_names = ['df_fund', 'df_market', 'df_train_fund', 'df_train_market', 'df_train_returns']
+    for df, df_name in zip(df_list, df_names):
+        print(f"Columns of {df_name} are: {list(df.columns)}")
+    
+
+
+data_preparation()
